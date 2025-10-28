b6042814742c7810566f8e01be809e1e21f4a31f
Thomas Serna <tasernas@eafit.edu.co>
2025-10-28 11:35:01 -0500

    verificacion enroque

--- src/FenParserMain.java
+++ src/FenParserMain.java
@@ -39,17 +39,6 @@ public class FenParserMain {
         // Metodo de prueba para clase parse.FenParser
         testParse("r1bqkbnr/pppp1Bpp/2n5/4p3/4P3/8/PPPP1PPP/RNBQK1NR b KQkq - 0 3");
 
-        /*
-                                 --- Casos de prueba ---
-
-        testParse("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-        testParse("rnbqk1nr/pppp1ppp/8/4p3/1b2P3/3P4/PPP2PPP/RNBQKBNR w KQkq - 1 3");
-        testParse("rnbqkbnr/pp1ppppp/2p5/8/3PP3/8/PPP2PPP/RNBQKBNR b KQkq - 0 2");
-        testParse("rnbqkbnr/pp1p1ppp/8/2pPp3/8/8/PPP1PPPP/RNBQKBNR w KQkq e6 0 3");
-        testParse("8/8/5k2/8/8/8/1K6/6R1 w - - 0 45");
-
-        */
-
 
     }
 

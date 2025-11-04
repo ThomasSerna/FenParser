5dc8512dc87da8c67c0f808e17c0370e0497d34b
Thomas Serna <tasernas@eafit.edu.co>
2025-11-03 20:54:27 -0500

    Integración entre backend y frontend

--- src/FenParserMain.java
+++ src/FenParserMain.java
@@ -4,14 +4,23 @@
 *
 */
 
+import gui.VentanaPrincipal;
 import parse.FenParser;
+import javax.swing.SwingUtilities;
 
 public class FenParserMain {
 
     public static void main(String[] args) {
+        // Iniciar la GUI en el hilo gráfico de Swing (buena práctica)
+        SwingUtilities.invokeLater(() -> { //lambda para crear un entorno de ejecución con esa libreria
+            VentanaPrincipal ventana = new VentanaPrincipal(); //crear ventana principal
+            ventana.setVisible(true); //hacerla visible
+        });
 
+        /*
         FenParser prueba = new FenParser();
         prueba.testParse("r1bqkbnr/pppp1Bpp/2n5/4p3/4P3/8/PPPP1PPP/RNBQK1NR b KQkq - 0 3");
+        */
 
     }
 

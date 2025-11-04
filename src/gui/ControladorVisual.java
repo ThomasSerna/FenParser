d7fd8c253f066e82702a30cfdfd64967cef1fb9c
Thomas Serna <tasernas@eafit.edu.co>
2025-11-03 19:52:25 -0500

    Actualización controladorVisual

--- src/gui/ControladorVisual.java
+++ src/gui/ControladorVisual.java
@@ -1,10 +1,10 @@
 /*
-*
-* Clase para gestionar la parte visual
-*
-* El paquete gui puede aumentar en la cantidad de clases
-*
-*/
+ *
+ * Clase para gestionar la parte visual
+ *
+ * El paquete gui puede aumentar en la cantidad de clases
+ *
+ */
 
 package gui;
 import javax.swing.*;
